f6535a78e375b212f3f79283362d9f133ae586c1
hanakusoman <>
2018-05-31 23:28:46 +0900

    iroiro2

--- app.js
+++ app.js
@@ -10,14 +10,97 @@ var app = new Vue({
 
     archives: [
         {
-            version: "1.9.3",
-            releaseNote: "note",
+            version: "1.9.4",
+            releaseNote: "releaseNote releaseNote",
             updatedAt: "2018/05/29 12:00:00",
             apps: [
                 {
-                    name: "k1",
-                    src: "k1",
-                }
+                    name: "dev",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "fut",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "STG K1",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "STG K3",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "google play dev",
+                    src: "",
+                    btnClass: "btn-success",
+                },
+                {
+                    name: "google play fut",
+                    src: "",
+                    btnClass: "btn-success",
+                },
+                {
+                    name: "google play STG K1",
+                    src: "",
+                    btnClass: "btn-success",
+                },
+                {
+                    name: "google play STG K3",
+                    src: "",
+                    btnClass: "btn-success",
+                }                                                      
+            ]
+        },        
+        {
+            version: "1.9.3",
+            releaseNote: "releaseNote releaseNote",
+            updatedAt: "2018/05/29 12:00:00",
+            apps: [
+                {
+                    name: "dev",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "fut",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "STG K1",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "STG K3",
+                    src: "",
+                    btnClass: "btn-warning",
+                },
+                {
+                    name: "google play dev",
+                    src: "",
+                    btnClass: "btn-success",
+                },
+                {
+                    name: "google play fut",
+                    src: "",
+                    btnClass: "btn-success",
+                },
+                {
+                    name: "google play STG K1",
+                    src: "",
+                    btnClass: "btn-success",
+                },
+                {
+                    name: "google play STG K3",
+                    src: "",
+                    btnClass: "btn-success",
+                }                                                      
             ]
         }
     ]
