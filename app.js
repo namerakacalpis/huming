6c3c47da9fb4b30ee9fdd24b8fa27c03252cc754
hanakusoman <>
2018-05-31 22:43:01 +0900

    iroiro

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+
+    
+    items: [
+        { message: 'Foo' },
+        { message: 'Bar' }
+    ],
+
+    archives: [
+        {
+            version: "1.9.3",
+            releaseNote: "note",
+            updatedAt: "2018/05/29 12:00:00",
+            apps: [
+                {
+                    name: "k1",
+                    src: "k1",
+                }
+            ]
+        }
+    ]
   }
 })
